f71e8e3cdc8d5b4ad5a52896f35de3cdc13a1c23
tayfurd <tayfurdemirayak0@gmail.com>
2026-04-28 07:58:15 +0300

    feat: basic calculator and palindromes detect function

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current, 1)
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = 1
+  for(i = a; i > 0; i--) {
+    result *= i
+  }
+  return result;
 };
 
 // Do not edit below this line
--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let replaceAll = word.replaceAll(/[.,?!:;'"` ()\-\[\]']/g, "")
+    let result = ""
+    for(i = replaceAll.length - 1; i >= 0; i--){
+        result += replaceAll.at(i)
+    }
+    
+    if (result.toLowerCase() === replaceAll.toLowerCase()) {return true}
+    else if (result.toLowerCase() !== replaceAll.toLowerCase()) {return false}
 };
+
+/* 
+toLowerCase
+delete commas and dots
+delete spaces
+
+replaceAll | //gi
+*/
 
 // Do not edit below this line
 module.exports = palindromes;
