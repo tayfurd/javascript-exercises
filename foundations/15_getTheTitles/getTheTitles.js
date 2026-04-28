4553c02fa4cbd133cfb695c3c8408a5a6d221b0a
tayfurd <tayfurdemirayak0@gmail.com>
2026-04-28 10:59:34 +0300

    feat: add function findTheOldest and getTheTitles

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    return array.map((element) => element.title)
 };
-
+/* 
+find the title in the array and return
+*/
 // Do not edit below this line
 module.exports = getTheTitles;
--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
 
+    let cloneArray = array.concat()
+
+    cloneArray.sort((person, person2) => {
+        if (person.yearOfDeath == undefined) {
+            person.yearOfDeath = parseInt(new Date().getFullYear())
+        }
+        else if (person2.yearOfDeath == undefined) {
+            person2.yearOfDeath = parseInt(new Date().getFullYear())
+        }
+
+       let age = person.yearOfDeath - person.yearOfBirth
+       let age2 = person2.yearOfDeath - person2.yearOfBirth
+       return age > age2 ? 1 : -1
+    })
+
+    return cloneArray[array.length-1]
 };
 
 // Do not edit below this line
